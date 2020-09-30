2a6fa7be3cf70ab39b44c7a9781e363f4acf441a
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2020-09-30 04:51:32 -0400

    Project Files

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField] private GameObject playerObject;
+    private Rigidbody playerRigidbody;
+    private Vector3 input = Vector3.zero;
+
+    #endregion
+
+    #region Monobehaviour Callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = playerObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody.MovePosition(playerRigidbody.position + input * 10 * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+
+    private void GetInput()
+    {
+        // Reset the vector each time we get input.
+        input = Vector3.zero;
+        // Using Horizontal and Vertical axes allows for use of controllers and easy control redefining with an Input Manager.
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/SplashDetection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashDetection : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField] private GameObject jumpPoint;
+    [SerializeField] private GameObject playerObject;
+    private Rigidbody playerRigidbody;
+
+    #endregion
+
+    #region Monobehaviour Callbacks
+
+    void Start()
+    {
+        playerRigidbody = playerObject.GetComponent<Rigidbody>();
+        goToJumpLocation();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("j"))
+        {
+            goToJumpLocation();
+        }
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    private void OnTriggerEnter()
+    {
+        goToJumpLocation();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void goToJumpLocation()
+    {
+        // Teleport the player to the jumpPoint
+        playerObject.transform.position = jumpPoint.transform.localPosition;
+        // Reset the player's rotation when they get to the jumpPoint
+        playerObject.transform.eulerAngles = Vector3.zero;
+        // Reset the player's velocity to 0 in all directions. Very Important!
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.angularVelocity = Vector3.zero;
+    }
+
+    #endregion
+}
